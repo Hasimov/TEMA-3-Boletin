6ed1a01501e9d3d85672ddc2809e1649e1d7ba02
Hasimov <jucarvazquez@gmail.com>
2014-11-27 10:25:23 +0100

    algunos cambios menores

--- TEMA 3 Boletin/src/ejercicio07/Ppal07.java
+++ TEMA 3 Boletin/src/ejercicio07/Ppal07.java
@@ -11,19 +11,18 @@ public class Ppal07 {
 		/*7. 	Crear un programa que emule una máquina vendedora de tickets de metro.
 				Tendremos que crear la clase ticket, la clase máquina y la clase principal o de
 				prueba. Podremos:
-				*Comprar uno o varios billetes teniendo en cuenta si hay que devolver
-				cambio o no.
+				*Comprar uno o varios billetes teniendo en cuenta si hay que devolver cambio o no.
 				*Imprimir por pantalla el billete.
-				*Además, al final de la jornada, el operario que lleva el mantenimiento de
-				la máquina, debe tener disponible un método que le ofrezca el saldo
-				total de la recaudación de la máquina en ese día (no es necesario usar
-				fechas) y ponga a cero el contador.
+				*Además, al final de la jornada, el operario que lleva el mantenimiento de la máquina, 
+				debe tener disponible un método que le ofrezca el saldo total de la recaudación de la máquina en ese día 
+				(no es necesario usar fechas) y ponga a cero el contador.
 				*Por último, también debe contar con la posibilidad de cambiar el precio
 				de los billetes ya que estos suelen subir todos los años.
 				*Las operaciones para el operario se harán solo si se introduce la
 				contraseña adecuada.*/
 		
-		int opc = 0;
+		int opc = 0, numticket,contraseña=1234;
+		
 
 		System.out.println("Bienvenido al servicio de venta de tickets automático del metro de Sevilla.\n¿Qué operación desea realizar?");
 
@@ -31,9 +30,14 @@ public class Ppal07 {
 		opc =Leer.datoInt();
 		
 		switch (opc) {
-		case 1:
+		case 1: System.out.println("¿Cuántos tickets desea comprar?");
+		      	numticket=Leer.datoInt();
 
 			break;
+		case 2: System.out.println("Introduzca la contraseña de administrador");
+      	contraseña=Leer.datoInt();
+
+      		break;
 
 		default:
 			break;
